920fb7862f8080fa004c1baccfaaa0f582b8188f
202144062@itc.ac.kr <202144062@itc.ac.kr>
2023-09-25 18:13:57 +0900

    week05_(1)

--- go/week05/main.go
+++ go/week05/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Guess number game!")
 	answer := rand.Intn(100) + 1 // 1~100
 	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+	inputScoreString, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputScoreString = strings.TrimSpace(inputScoreString)      // remove space
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // string to 32bit float
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
