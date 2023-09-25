e256fd99b956cd62d77b60df50dadd726816438d
202144062@itc.ac.kr <202144062@itc.ac.kr>
2023-09-25 17:59:10 +0900

    v2.0

--- go/week05/main.go
+++ go/week05/main.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Input score : ")
-	reader := bufio.NewReader(os.Stdin)
-	inputScoreString, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	inputScoreString = strings.TrimSpace(inputScoreString)      // remove space
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // string to 32bit float
-	if err != nil {
-		log.Fatal(err)
-	}
-	var grade string
-	if inputScore >= 90 { // invalid operation: inputScore >= 90 (mismatched types string and untyped int)
-		grade = "A Grade!"
-		// fmt.Println("You got", grade)
-	} else {
-		grade = "under A Grade..."
-		// fmt.Println("You got", grade)
-	}
-	fmt.Println(grade)
+	fmt.Println("Guess number game!")
+	answer := rand.Intn(100) + 1 // 1~100
+	fmt.Println(answer)
 
-	// var now time.Time
-	// now = time.Now()
-	// var year = now.Year()
-	// month := now.Month()
-	// fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
 }
