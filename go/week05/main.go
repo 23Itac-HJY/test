59680b1dca42fa8ffd8c4aa9d1477676c9f7655a
202144062@itc.ac.kr <202144062@itc.ac.kr>
2023-09-25 17:20:27 +0900

    v0.5 if

--- go/week05/main.go
+++ go/week05/main.go
@@ -10,8 +10,10 @@ import (
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') // option 2
-	log.Fatal(err)                             // 프로그램 종료해서 점수 출력 안됨
+	inputScore, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(inputScore)
 
 	// var now time.Time
