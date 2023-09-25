4b9280031b6a06194cbd110f623f8948954910ee
202144062@itc.ac.kr <202144062@itc.ac.kr>
2023-09-25 16:46:23 +0900

    week05 v0.1

---
+++ go/week05/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var now time.Time
+	now = time.Now()
+	var year = now.Year()
+	month := now.Month()
+	fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
+}
