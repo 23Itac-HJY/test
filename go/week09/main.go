6cace8f99d10e7fc8f24e19313a861e6707753de
HJY <202144062@itc.ac.kr>
2023-10-23 17:35:28 +0900

    week09-02

--- go/week09/main.go
+++ go/week09/main.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	a := 10
-	var pa *int
-	pa = &a
-
-	fmt.Println(a, *pa)
-	fmt.Println(&a, pa)
-	fmt.Printf("%T %T %T %T\n", a, *pa, &a, pa)
-	fmt.Println(&pa)
-
+func double(n *int) {
+	*n = *n * 2
 }
+func main() {
+	var a int = 5
+	double(&a) // pass by pointer
+	fmt.Printf("%d\n", a)
+}
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	a := 10
+// 	var pa *int
+// 	pa = &a
+
+// 	fmt.Println(a, *pa)
+// 	fmt.Println(&a, pa)
+// 	fmt.Printf("%T %T %T %T\n", a, *pa, &a, pa)
+// 	fmt.Println(&pa)
+
+// }
