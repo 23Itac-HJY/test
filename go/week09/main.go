f2bda092ccec4a0017bbc0edf2ed9742131c1bdf
HJY <202144062@itc.ac.kr>
2023-10-23 17:43:38 +0900

    week09-03

--- go/week09/main.go
+++ go/week09/main.go
@@ -2,14 +2,33 @@ package main
 
 import "fmt"
 
-func double(n *int) {
-	*n = *n * 2
+func swap(n1 *int, n2 *int) { // 두 수를 교환
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+
 }
 func main() {
-	var a int = 5
-	double(&a) // pass by pointer
-	fmt.Printf("%d\n", a)
+	var a int = 10
+	b := 20
+	c := &a // var c *int = &a
+
+	//fmt.Printf("%d %d %x\n", a, b, c)
+	fmt.Printf("%d %d %d\n", a, b, *c)
 }
+
+// package main
+
+// import "fmt"
+
+// func double(n *int) {
+// 	*n = *n * 2
+// }
+// func main() {
+// 	var a int = 5
+// 	double(&a) // pass by pointer
+// 	fmt.Printf("%d\n", a)
+// }
 
 // package main
 
