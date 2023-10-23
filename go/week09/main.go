abdca7f6770e241cdd9c95dbffcfa593e69545b3
HJY <202144062@itc.ac.kr>
2023-10-23 17:50:26 +0900

    week09-04

--- go/week09/main.go
+++ go/week09/main.go
@@ -14,7 +14,12 @@ func main() {
 	c := &a // var c *int = &a
 
 	//fmt.Printf("%d %d %x\n", a, b, c)
-	fmt.Printf("%d %d %d\n", a, b, *c)
+	//fmt.Printf("%d %d %d\n", a, b, *c)
+	fmt.Printf("%T\n", c)
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
+	fmt.Println(*c, b)
 }
 
 // package main
