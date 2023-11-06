4eb9f12bcb9132c08f1a9a685d38fd6992577928
HJY <202144062@itc.ac.kr>
2023-11-06 17:15:31 +0900

    week11 -02 error

--- go/week11/main.go
+++ go/week11/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// var primes [3]int
@@ -13,4 +15,20 @@ func main() {
 	primes := [3]int{2, 3, 5}
 
 	fmt.Println(primes, primes[1])
+
+	test := [5]bool{true, true, true} // 초기화하지 않은 배열 원소 나머지 두 값에 제로값(false) 할당
+	fmt.Println(test[3])
+
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Println(primes[i])
+	// }
+
+	// fmt.Println(primes[3]) // invalid argument: index 3 out of bounds [0:3]
+
+	i := 0
+	for i < 4 { //panic: runtime error: index out of range [3] with length 3
+		fmt.Println(primes[i])
+		i++
+	}
+
 }
