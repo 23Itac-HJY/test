dd0634005032c18daf2d6332f497c4b64c3866c8
HJY <202144062@itc.ac.kr>
2023-11-06 17:04:48 +0900

    week11 - 01 array

--- go/week11/main.go
+++ go/week11/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	// var primes [3]int
+	// primes[0] = 2
+	// primes[1] = 3
+	// primes[2] = 5
+
+	// var primes [3]int = [3]int{2, 3, 5} //배열 리터럴로 배열 초기화
+
+	primes := [3]int{2, 3, 5}
+
+	fmt.Println(primes, primes[1])
 }
