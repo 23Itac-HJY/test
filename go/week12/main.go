3d15108faa47e9f5d3770c152fbf2c327f7341cf
HJY <202144062@itc.ac.kr>
2023-11-13 17:51:15 +0900

    week12 -06

--- go/week12/main.go
+++ go/week12/main.go
@@ -3,14 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	a := []string{"a", "b", "c", "d"}
-	as := a[:2]
-	as[1] = "z"
-	fmt.Println(a, as)
+	a := make([]string, 4, 5) //type, length, ,capacity
+	a[0] = "a"
+	a[2] = "c"
+	a[3] = "d"
+	as := a[0:2]
+	as[1] = "Z"
+	c := append(a, "y")
+	//c := append(a, "y", "x")
 
-	b := [4]int{4, 3, 2, 1}
-	bs := b[1:3]
-	fmt.Println(b, bs)
+	c[0] = "Q"
+
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
+
+	// a := []string{"a", "b", "c", "d"}
+	// as := a[:2]
+	// as[1] = "z"
+	// fmt.Println(a, as)
+
+	// b := [4]int{4, 3, 2, 1}
+	// bs := b[1:3]
+	// fmt.Println(b, bs)
 }
 
 // func main() {
